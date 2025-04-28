230a5d47666899cb7de26274b13400be4cf8b022
Charles Amen <charlesamenlokonon@gmail.com>
2025-04-28 23:58:52 +0100

    Start with register page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import {Input} from "@/components/ui/input";
+import {Label} from "@/components/ui/label";
+
 
 export default function Home() {
     return (
-        <div className={'bg-sec'}>Bonjour les amis !</div>
+        <>
+            <form action="">
+
+            </form>
+        </>
     );
 }
