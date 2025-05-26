2aa84aa00af81eae6897d628c33a9c57865f96fe
Charles Amen <charlesamenlokonon@gmail.com>
2025-05-26 05:20:00 +0100

    Delete props in register-form component to fix build error in deployment

--- src/components/custom/resgister-form.tsx
+++ src/components/custom/resgister-form.tsx
@@ -28,7 +28,7 @@ export function ResgisterForm({
     }
 
     return (
-        <div className={cn("flex flex-col gap-6", className)} {...props}>
+        <div className={cn("flex flex-col gap-6", className)}>
             <div className="flex flex-col items-center gap-2 text-center">
                 <h1 className="text-2xl font-bold">Créer un compte</h1>
                 <p className="text-balance text-sm text-muted-foreground">
